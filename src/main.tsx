15e6b106fd1e3f844d2418e2354c1f6b0b474d0c
muradalshorman987 <murad.alshorman@infograph.com.jo>
2024-01-03 15:29:12 +0300

    downloading use query hook

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import theme from "./Theme"
+import theme from "./Theme";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+const queryCLient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ChakraProvider  theme={theme}>
-      <ColorModeScript initialColorMode={theme.config.initialColorMode}/>
-      <App />
-    </ChakraProvider>
+    <QueryClientProvider client={queryCLient}>
+      <ChakraProvider theme={theme}>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+        <App />
+      </ChakraProvider>
+    </QueryClientProvider>
   </React.StrictMode>,
 );
